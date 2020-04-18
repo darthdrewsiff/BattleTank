f96e3bd266443ed3cd36f59fb7ceff3aaae8e09e
drewsiff0880 <darthdrewsiff@gmail.com>
2020-04-18 16:11:47 -0500

    BlueprintCallable()

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -14,6 +14,11 @@ ATank::ATank()
 
 }
 
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
+}
+
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
@@ -40,3 +45,4 @@ void ATank::AimAt(FVector HitLocation)
 	TankAimingComponent->AimAt(HitLocation);
 	
 }
+
